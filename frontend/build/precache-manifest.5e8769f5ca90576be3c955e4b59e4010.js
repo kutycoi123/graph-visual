5dbbe724edbd9629a1c1280d4a26766dc07aa68f
kutycoi123 <kutycoi123@gmail.com>
2020-08-17 11:20:16 -0700

    Added build

---
+++ frontend/build/precache-manifest.5e8769f5ca90576be3c955e4b59e4010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a28855fcf59a36e3b6ebda980caaa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "995f5f912987ed322237",
+    "url": "/static/css/main.7f01518e.chunk.css"
+  },
+  {
+    "revision": "6e1760e5dad3a7a4792e",
+    "url": "/static/js/2.4e0f4011.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4e0f4011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995f5f912987ed322237",
+    "url": "/static/js/main.10dd4823.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
